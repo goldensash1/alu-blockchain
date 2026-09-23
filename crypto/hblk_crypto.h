9b0bf8b4303f9441e74e6aff0e6f9a6e1a88471e
Golden Sash <101018402+goldensash01@users.noreply.github.com>
2026-09-23 19:57:52 +0200

    Include ecdsa/pem/std headers in hblk_crypto.h for checker helpers

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -3,9 +3,19 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <openssl/sha.h>
 #include <openssl/ec.h>
+#include <openssl/ecdsa.h>
 #include <openssl/obj_mac.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
 
 /**
  * EC_CURVE - Elliptic curve used by this project
